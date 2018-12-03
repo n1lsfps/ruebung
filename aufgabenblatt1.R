a71a0b2cfa152fd85d267253c33011cf1bb08265
Nils <nilsaurelburkhardt@gmail.com>
2018-12-03 15:57:49 +0100

    kommentare hinzugefuegt

--- aufgabenblatt1.R
+++ aufgabenblatt1.R
@@ -1,18 +1,32 @@
+# Vektor y generieren 
+
 y <- sample(c(seq(from=1, to=100)), sample(1:10, 1, replace=FALSE)) 
 
-laengey <- length(y)
+# laenge des y Vektors auf lengthy speichern
 
-x <- sample(c(seq(from=1, to=laengey)), sample(1:10, 1, replace=FALSE))
+lengthy <- length(y)
+
+# Vektor x generieren mit maximum von lenghty
+
+x <- sample(c(seq(from=1, to=lengthy)), sample(1:10, 1, replace=FALSE))
+
+# multiplizieren von x und y 
 
 x*y
 
+# n auf Summe von lanegen von x und y setzen 
+
 n <- length(x) + length(y) 
+
+# Matrix erstellen
 
 m1 <-matrix(x,y, ncol=n)
 
 m1
 
 m2 <- m1
+
+# Matrix multiplikation
 
 m3 <- m1*m2
 
